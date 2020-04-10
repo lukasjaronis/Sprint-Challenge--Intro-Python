680276c20937a97f055e1ef3fc8adf2750758c37
Lukas Jaronis <jaronisluke13@gmail.com>
2020-04-10 12:26:35 -0400

    Sprint Complete

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,34 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    pass
+
+
+# GroundVehicle from BASE => class Vehicle
+class GroundVehicle(Vehicle):
+    pass
+
+
+# Car => class GroundVehicle => BASE class Vehicle
+class Car(GroundVehicle):
+    pass
+
+
+# Motorcycle => class GroundVehicle => BASE class Vehicle
+class Motorcycle(GroundVehicle):
+    pass
+
+
+# FlightVehicle from BASE => class Vehicle
+class FlightVehicle(Vehicle):
+    pass
+
+# Starship => class FlightVehicle => BASE class Vehicle
+class Starship(FlightVehicle):
+    pass
+
+# Airplane => class FlightVehicle => BASE class Vehicle
+class Airplane(FlightVehicle):
+    pass
